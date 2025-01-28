cb4deca41123a9e5b0191a53b052ae9dc644c270
benjaminbihr <benjamin.bihr@gmail.com>
2025-01-28 13:58:44 +0100

    Fix carousel

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -1,39 +1,42 @@
 "use client";
 
-import React from 'react';
-import useEmblaCarousel from 'embla-carousel-react';
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
 
 const ImageCarousel = () => {
-     const [emblaRef] = useEmblaCarousel({
-     loop: true,
-     align: 'center',
-     skipSnaps: false,
-     },  [Autoplay()]);
-    const images = [
-     '/grand_palais_01.jpg',
-     '/grand_palais_02.jpg',
-     '/grand_palais_03.jpg',
-     '/grand_palais_04.jpg',
-     ];
-      return (
-         <div className="embla" ref={emblaRef}>
-           <div className="embla__container">
-           {images.map((image, index) => (
-             <div key={index} className="embla__slide">
-               <Image
-                  className="embla__slide__img rounded-lg" 
-                  src={image} 
-                  alt={`Slide ${index + 1}`} 
-                  width={250}
-                  height={100}
-                  priority
-                />
-             </div>
-           ))}
-           </div>
-         </div>
-       );
+  const [emblaRef] = useEmblaCarousel(
+    {
+      loop: true,
+      align: "center",
+      skipSnaps: false,
+    },
+    [Autoplay()]
+  );
+  const images = [
+    "grand_palais_01.jpg",
+    "grand_palais_02.jpg",
+    "grand_palais_03.jpg",
+    "grand_palais_04.jpg",
+  ];
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {images.map((image, index) => (
+          <div key={index} className="embla__slide">
+            <Image
+              className="embla__slide__img rounded-lg"
+              src={image}
+              alt={`Slide ${index + 1}`}
+              width={250}
+              height={100}
+              priority
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default ImageCarousel;
