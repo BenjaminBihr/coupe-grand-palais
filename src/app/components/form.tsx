43ca113832e032879dbee4c726afc4534d07f1ed
benjaminbihr <benjamin.bihr@gmail.com>
2025-01-28 23:38:14 +0100

    Fix form

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -29,6 +29,7 @@ export default function Form() {
           <input
             type="email"
             id="email"
+            name="email"
             className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500 dark:shadow-xs-light"
             placeholder="email@example.com"
             required
@@ -43,6 +44,7 @@ export default function Form() {
           </label>
           <textarea
             id="message"
+            name="message"
             rows={8}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
             placeholder="Leave a comment..."
